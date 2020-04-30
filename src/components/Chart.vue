6529878db8f8cb15d0638ba618ab82ae559437f7
Adrian Santo Pasqualini <aspasqua@edu.uwaterloo.ca>
2020-04-30 16:14:24 -0400

    Added support for optional prerequitsites

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -38,17 +38,41 @@ export default {
       series.dataFields.id = "name";
       series.dataFields.linkWith = "link";
       // Add labels
-      series.nodes.template.label.text = "{name}";
-      series.nodes.template.tooltipText = "{courseData.title}";
+      series.nodes.template.label.text = "[bold]{name}[/]";
+      series.nodes.template.tooltipText = "{rawPrerequisites}";
       series.fontSize = 10;
       series.minRadius = 15;
       series.maxRadius = 40;
       series.centerStrength = 0.5;
     },
 
+    numberToString(num) {
+      switch (num) {
+        case 1:
+          return "One Of:";
+        case 2:
+          return "Two Of:";
+        case 3:
+          return "Three Of:";
+        case 4:
+          return "Four Of:";
+        case 5:
+          return "Five Of:";
+        case 6:
+          return "Six Of:";
+        case 7:
+          return "Seven Of:";
+        case 8:
+          return "Eight Of:";
+        default:
+          return "Num Too High";
+      }
+    },
+
     async generateCourseTree(course) {
       var newCourse = {
             name: course.subject + course.catalog_number,
+            rawPrerequisites: "No Prerequisites",
             value: 1,
             courseData: course,
             children: [],
@@ -56,37 +80,53 @@ export default {
       await axios.get("https://api.uwaterloo.ca/v2/courses/"+course.subject+"/"+course.catalog_number+"/prerequisites.json?key="+UW_API_KEY)
       .then(async response => {
           if (response.data.data.prerequisites_parsed) {
-            newCourse.children = newCourse.children.concat(await this.generatePrereqChildren(response.data.data.prerequisites_parsed));
-            console.log(newCourse.children);
+            newCourse.rawPrerequisites = response.data.data.prerequisites;
+            newCourse.children = newCourse.children.concat(await this.generatePrereqChildrenFromList(response.data.data.prerequisites_parsed));
           }          
       });
       return newCourse;
     },
 
-    async generatePrereqChildren(prerequisites_parsed) {
+    async generatePrereqChildrenFromList(prerequisites_parsed) {
       var prereqChildren = [];
-      for (const prereq of prerequisites_parsed) {
-        if (typeof prereq === "string") {
-          prereqChildren = [...prereqChildren, await this.generateCourseTree(this.courseList.get(prereq)[0])];
+
+      if (typeof prerequisites_parsed[0] === "number") {
+        return this.generatePrereqChildrenSingleTerm(prerequisites_parsed);
+      } else {
+        for (const prereq of prerequisites_parsed) {
+          prereqChildren = [...prereqChildren, await this.generatePrereqChildrenSingleTerm(prereq)];
         }
       }
-      console.log(prereqChildren)
       return prereqChildren;
-    }
+    },
+
+    async generatePrereqChildrenSingleTerm(prereqTerm) {
+      if (typeof prereqTerm === "string") {
+        return await this.generateCourseTree(this.courseList.get(prereqTerm)[0])
+        .catch(err => console.log("Couldnt find course: ", err));
+      } else {
+        let courseOptions = {
+          name: this.numberToString(prereqTerm[0]),
+          rawPrerequisites: "Choose one of these courses:",
+          value: 1,
+          children: []
+        }
+        for (let i = 1; i < prereqTerm.length; i++) {
+          const newChild = await this.generatePrereqChildrenSingleTerm(prereqTerm[i])
+          courseOptions.children = [...courseOptions.children, newChild];
+        }
+        return courseOptions;
+      }
+    },
   },
-
-
 
   watch: {
     selectedCourses: async function(newCourses) {
-      console.log("Start");
       let newChartData = [];
       for (const course of newCourses) {
           var newCourse = await this.generateCourseTree(course);
           newChartData = [...newChartData, newCourse]
       }
-      
-      console.log("End");
       this.chartData = newChartData;
       await this.updateChart();
       
